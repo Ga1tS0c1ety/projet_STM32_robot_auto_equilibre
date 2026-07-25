8649c53caa39c58a31f3f8ca677987cbfb067595
gaitSociety <binlife0@gmail.com>
2026-07-25 09:58:46 +0200

    implementation des fonctionnalité i2c

--- seance3/i2c/Src/main.c
+++ seance3/i2c/Src/main.c
@@ -14,6 +14,60 @@
 #define GPIOB_PUPDR        (*(volatile uint32_t *)(GPIOB_BASE + 0x0CUL))
 #define GPIOB_AFRH         (*(volatile uint32_t *)(GPIOB_BASE + 0x24UL))
 
+#define RCC_APB1ENR        (*(volatile uint32_t *)(RCC_BASE + 0x40UL))
+
+#define RCC_APB1ENR_I2C1EN (1UL << 21)
+
+#define I2C1_BASE          0x40005400UL
+
+#define I2C1_CR1           (*(volatile uint32_t *)(I2C1_BASE + 0x00UL))
+#define I2C1_CR2           (*(volatile uint32_t *)(I2C1_BASE + 0x04UL))
+
+#define I2C1_CCR    (*(volatile uint32_t *)(I2C1_BASE + 0x1CUL))
+
+#define I2C1_TRISE    (*(volatile uint32_t *)(I2C1_BASE + 0x20UL))
+
+#define I2C1_CR1    (*(volatile uint32_t *)(I2C1_BASE + 0x00UL))
+#define I2C_CR1_PE  (1UL << 0)
+
+#define I2C1_DR     (*(volatile uint32_t *)(I2C1_BASE + 0x10UL))
+#define I2C1_SR1    (*(volatile uint32_t *)(I2C1_BASE + 0x14UL))
+#define I2C1_SR2    (*(volatile uint32_t *)(I2C1_BASE + 0x18UL))
+
+#define I2C_CR1_ACK      (1UL << 10)
+
+#define I2C_SR1_BTF      (1UL << 2)
+#define I2C_SR1_RXNE     (1UL << 6)
+#define I2C_SR1_TXE      (1UL << 7)
+
+/* Bits du registre I2C_CR1 */
+#define I2C_CR1_START    (1UL << 8)
+#define I2C_CR1_STOP     (1UL << 9)
+
+/* Bits du registre I2C_SR1 */
+#define I2C_SR1_SB       (1UL << 0)
+#define I2C_SR1_ADDR     (1UL << 1)
+#define I2C_SR1_AF       (1UL << 10)
+
+/* Bits du registre I2C_SR2 */
+#define I2C_SR2_BUSY     (1UL << 1)
+
+#define I2C_TIMEOUT      1000000UL
+
+static void i2c1_configure_input_clock(void)
+{
+    /*
+     * CR2[5:0] contient la fréquence de l'horloge APB1,
+     * exprimée en MHz.
+     *
+     * PCLK1 = 16 MHz
+     * FREQ  = 16
+     */
+
+    I2C1_CR2 &= ~0x3FUL;
+    I2C1_CR2 |= 16UL;
+}
+
 static void i2c1_gpio_init(void)
 {
     /* 1. Activer l'horloge du port GPIOB */
@@ -70,16 +124,470 @@ static void i2c1_gpio_init(void)
     GPIOB_AFRH |=  ((4UL << 0) | (4UL << 4));
 }
 
+static void i2c1_clock_enable(void)
+{
+    /*
+     * Activer l’horloge du périphérique I2C1.
+     *
+     * I2C1EN correspond au bit 21 de RCC_APB1ENR.
+     */
+    RCC_APB1ENR |= RCC_APB1ENR_I2C1EN;
+
+    /*
+     * Lecture de synchronisation pour laisser
+     * l’activation de l’horloge se propager.
+     */
+    (void)RCC_APB1ENR;
+}
+
+static void i2c1_configure_bus_speed(void)
+{
+    /*
+     * Mode standard :
+     * FS = 0
+     *
+     * PCLK1 = 16 MHz
+     * SCL   = 100 kHz
+     *
+     * CCR = 16 000 000 / (2 × 100 000)
+     * CCR = 80
+     */
+
+    I2C1_CCR = 80UL;
+}
+
+static void i2c1_configure_rise_time(void)
+{
+    /*
+     * Mode standard à 100 kHz.
+     *
+     * PCLK1 = 16 MHz
+     * TRISE = 16 + 1 = 17
+     */
+    I2C1_TRISE = 17UL;
+}
+
+static void i2c1_enable(void)
+{
+    /*
+     * PE = Peripheral Enable.
+     *
+     * PE = 1 : périphérique I2C1 activé.
+     * PE = 0 : périphérique I2C1 désactivé.
+     */
+    I2C1_CR1 |= I2C_CR1_PE;
+}
+
+static uint32_t i2c1_probe(uint8_t adresse_7_bits)
+{
+    uint32_t timeout;
+
+    /*
+     * 1. Vérifier que le bus est libre.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while ((I2C1_SR2 & I2C_SR2_BUSY) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        return 1;
+    }
+
+    /*
+     * 2. Demander une condition START.
+     */
+    I2C1_CR1 |= I2C_CR1_START;
+
+    /*
+     * 3. Attendre que START soit généré.
+     *
+     * SB = Start Bit.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_SB) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        return 2;
+    }
+
+    /*
+     * 4. Envoyer l'adresse du périphérique.
+     *
+     * Décalage à gauche :
+     * - bits [7:1] : adresse ;
+     * - bit  [0]   : R/W = 0 pour écriture.
+     */
+    I2C1_DR = ((uint32_t)adresse_7_bits << 1);
+
+    /*
+     * 5. Attendre soit :
+     * - ADDR : le périphérique répond ;
+     * - AF   : le périphérique ne répond pas.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & (I2C_SR1_ADDR | I2C_SR1_AF))
+           && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 3;
+    }
+
+    /*
+     * AF = Acknowledge Failure.
+     * Le périphérique n'a pas envoyé d'ACK.
+     */
+    if (I2C1_SR1 & I2C_SR1_AF)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+
+        /* Effacer le drapeau AF. */
+        I2C1_SR1 &= ~I2C_SR1_AF;
+
+        return 4;
+    }
+
+    /*
+     * Le périphérique a répondu.
+     *
+     * La lecture successive de SR1 puis SR2
+     * efface le drapeau ADDR.
+     */
+    (void)I2C1_SR1;
+    (void)I2C1_SR2;
+
+    /*
+     * Nous terminons ce simple test.
+     */
+    I2C1_CR1 |= I2C_CR1_STOP;
+
+    return 0;
+}
+
+static uint32_t i2c1_read_register(uint8_t adresse_7_bits,
+                                   uint8_t registre,
+                                   uint8_t *valeur)
+{
+    uint32_t timeout;
+
+    /*
+     * 1. Attendre que le bus soit libre.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while ((I2C1_SR2 & I2C_SR2_BUSY) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        return 1;
+    }
+
+    /*
+     * 2. Générer START.
+     */
+    I2C1_CR1 |= I2C_CR1_START;
+
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_SB) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        return 2;
+    }
+
+    /*
+     * 3. Envoyer l’adresse du MPU6050 en écriture.
+     *
+     * Bit R/W = 0.
+     */
+    I2C1_DR = ((uint32_t)adresse_7_bits << 1);
+
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & (I2C_SR1_ADDR | I2C_SR1_AF))
+           && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 3;
+    }
+
+    if (I2C1_SR1 & I2C_SR1_AF)
+    {
+        I2C1_SR1 &= ~I2C_SR1_AF;
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 4;
+    }
+
+    /*
+     * Effacer ADDR par une lecture de SR1 puis SR2.
+     */
+    (void)I2C1_SR1;
+    (void)I2C1_SR2;
+
+    /*
+     * 4. Attendre que DR soit prêt,
+     * puis envoyer l’adresse interne du registre.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_TXE) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 5;
+    }
+
+    I2C1_DR = registre;
+
+    /*
+     * Attendre la transmission complète du registre.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_BTF) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 6;
+    }
+
+    /*
+     * 5. Générer un REPEATED START.
+     */
+    I2C1_CR1 |= I2C_CR1_START;
+
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_SB) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 7;
+    }
+
+    /*
+     * 6. Envoyer l’adresse du MPU6050 en lecture.
+     *
+     * Bit R/W = 1.
+     */
+    I2C1_DR = ((uint32_t)adresse_7_bits << 1) | 1UL;
+
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & (I2C_SR1_ADDR | I2C_SR1_AF))
+           && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 8;
+    }
+
+    if (I2C1_SR1 & I2C_SR1_AF)
+    {
+        I2C1_SR1 &= ~I2C_SR1_AF;
+        I2C1_CR1 |= I2C_CR1_STOP;
+        return 9;
+    }
+
+    /*
+     * 7. Réception d’un seul octet.
+     *
+     * ACK = 0 indique que nous ne voulons
+     * recevoir qu’un seul octet.
+     */
+    I2C1_CR1 &= ~I2C_CR1_ACK;
+
+    /*
+     * Effacer ADDR en lisant SR1 puis SR2.
+     */
+    (void)I2C1_SR1;
+    (void)I2C1_SR2;
+
+    /*
+     * Demander STOP avant la réception finale.
+     */
+    I2C1_CR1 |= I2C_CR1_STOP;
+
+    /*
+     * 8. Attendre l’octet reçu.
+     */
+    timeout = I2C_TIMEOUT;
+
+    while (!(I2C1_SR1 & I2C_SR1_RXNE) && (timeout > 0))
+    {
+        timeout--;
+    }
+
+    if (timeout == 0)
+    {
+        return 10;
+    }
+
+    /*
+     * Lire la valeur reçue.
+     */
+    *valeur = (uint8_t)I2C1_DR;
+
+    /*
+     * Réactiver ACK pour les futures opérations.
+     */
+    I2C1_CR1 |= I2C_CR1_ACK;
+
+    return 0;
+}
+
 int main(void)
 {
     uart_init();
 
-    uart_send_string("Demarrage du robot\r\n");
+    uart_send_string("\r\nDemarrage du robot\r\n");
     uart_send_string("Configuration des broches I2C...\r\n");
 
     i2c1_gpio_init();
 
     uart_send_string("PB8 et PB9 configurees en AF4\r\n");
+    uart_send_string("Activation de l'horloge I2C1...\r\n");
+
+
+    i2c1_clock_enable();
+
+    if (RCC_APB1ENR & RCC_APB1ENR_I2C1EN)
+    {
+        uart_send_string("Horloge I2C1 activee\r\n");
+    }
+    else
+    {
+        uart_send_string("Erreur : horloge I2C1 inactive\r\n");
+    }
+
+
+    i2c1_configure_input_clock();
+
+    uart_send_string("CR2 FREQ = ");
+    uart_send_uint(I2C1_CR2 & 0x3FUL);
+    uart_send_string(" MHz\r\n");
+
+    i2c1_configure_bus_speed();
+
+    uart_send_string("CCR = ");
+    uart_send_uint(I2C1_CCR & 0x0FFFUL);
+    uart_send_string("\r\n");
+
+    i2c1_configure_rise_time();
+
+    uart_send_string("TRISE = ");
+    uart_send_uint(I2C1_TRISE & 0x3FUL);
+    uart_send_string("\r\n");
+
+    i2c1_enable();
+
+    if (I2C1_CR1 & I2C_CR1_PE)
+    {
+        uart_send_string("I2C1 active\r\n");
+    }
+    else
+    {
+        uart_send_string("Erreur : I2C1 inactif\r\n");
+    }
+
+    uint32_t resultat;
+
+    uart_send_string("Test de l'adresse I2C 0x68...\r\n");
+
+    resultat = i2c1_probe(0x68);
+
+    if (resultat == 0)
+    {
+        uart_send_string("MPU6050 detecte a l'adresse 0x68\r\n");
+    }
+    else if (resultat == 1)
+    {
+        uart_send_string("Erreur : bus I2C occupe\r\n");
+    }
+    else if (resultat == 2)
+    {
+        uart_send_string("Erreur : START non genere\r\n");
+    }
+    else if (resultat == 3)
+    {
+        uart_send_string("Erreur : timeout pendant l'adressage\r\n");
+    }
+    else
+    {
+        uart_send_string("Erreur : aucune reponse a l'adresse 0x68\r\n");
+    }
+
+    uint8_t identifiant;
+    uint32_t erreur;
+
+    uart_send_string("Lecture du registre WHO_AM_I...\r\n");
+
+    erreur = i2c1_read_register(0x68, 0x75, &identifiant);
+
+    if (erreur == 0)
+    {
+        uart_send_string("WHO_AM_I = ");
+        uart_send_uint(identifiant);
+        uart_send_string(" en decimal\r\n");
+
+        if (identifiant == 0x68)
+        {
+            uart_send_string("MPU6050 correctement identifie\r\n");
+        }
+        else
+        {
+            uart_send_string("Attention : identifiant inattendu\r\n");
+        }
+    }
+    else
+    {
+        uart_send_string("Erreur de lecture I2C, code = ");
+        uart_send_uint(erreur);
+        uart_send_string("\r\n");
+    }
+
 
     while (1)
     {
